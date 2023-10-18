30a1e1b5c015b8964a06c09d20fe83aade9066d5
rajat <rajatrandai232@gmail.com>
2023-10-18 15:58:15 +0530

    first commit

--- components/Header.js
+++ components/Header.js
@@ -9,7 +9,8 @@ import { db } from '@/app/shared/firebaseConfig';
 export default function Header() {
     const path = usePathname();
     const router = useRouter();
-    const { userData, setUserData, setCount } = useContext(DataContext);
+    const { setCount } = useContext(DataContext);
+    const [userData, setUserData] = useState([]);
 
     useEffect(() => {
         const user = JSON.parse(localStorage.getItem("user"));
@@ -29,9 +30,9 @@ export default function Header() {
             const q = query(collection(db, "users"), where("email", "==", user?.email));
             const querySnapShot = await getDocs(q);
             querySnapShot.forEach((doc) => {
-                setUserData(doc.data());
+                setUserData(doc?.data());
+                console.log("data",doc.data())
                 localStorage.setItem("count", doc.data()?.messages);
-                setCount(doc.data()?.messages);
             });
         }
     }
@@ -39,7 +40,7 @@ export default function Header() {
     return (
         <div className="container w-full items-center gap-5 p-2 flex flex-row border-b-2 border-white">
             <Link href={'/'} className='text-2xl md:text-3xl font-bold'>N E X T A G R A M</Link>
-            <Link href={'/profile'} className='w-fit me-3 ms-auto'><img className='w-12 h-12 rounded-full' src={userData?.profileImg} alt="Profile" /></Link>
+            <Link href={'/profile'} className='w-fit me-3 ms-auto'><img className='w-12 h-12 rounded-full' src={userData?.profileImg} alt={userData.username} /></Link>
         </div>
     );
 }
